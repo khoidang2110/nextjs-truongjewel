962275412f92057f9bb73178590752d22892a7fe
khoidang2110 <khoidang2110@gmail.com>
2024-03-29 23:46:53 +0700

    button back

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -12,24 +12,24 @@ console.log(params.id)
 const currentAlbum = projects[Number(params.id)-1]
   return (
     <section >
-      
+      <div className='pt-10'>
       <Link href={currentAlbum.link} className="">
-            <p className=' ml-10 capitalize opacity-70 fixed pt-10'>{currentAlbum.type}:
+            <p className=' ml-10 capitalize  fixed  bg-cyan-950 text-white  py-2 px-4 border rounded-lg shadow'>{currentAlbum.type}
 
 </p>
               </Link>
-
+      </div>
 
       
       <div className='text-center pt-20 ' >
-        <div className='fixed bg-white w-full'>
+        <div className=' bg-white w-full'>
         <h1 className='text-2xl  '>
 {currentAlbum.title}</h1>
       <p className='pl-10'>{currentAlbum.description}</p>
         </div>
 
       
-      <Album projects={currentAlbum} direct='flex-col pt-20'/>
+      <Album projects={currentAlbum} direct='flex-col pt-12'/>
       </div>
 
     </section>
