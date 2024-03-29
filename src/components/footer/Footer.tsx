8dc7e0fa80ff60896142e7cdbcd13b44c3c39cc4
khoidang2110 <khoidang2110@gmail.com>
2024-03-29 22:57:54 +0700

    phone

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = () => {
     return (
       <footer className="bg-white text-center py-5">
-        <p>Gửi mẫu qua fb hoặc zalo:0909090909 </p>
+        <p>Gửi mẫu qua fb hoặc zalo:0345.983.217 </p>
       </footer>
     );
   }
